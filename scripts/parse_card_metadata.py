38661baf905f98eb14f51e472a726795e3328a4c
Anthony Underwood <au3@sanger.ac.uk>
2021-10-13 07:44:17 +0000

    parse card metadata correctly

--- scripts/parse_card_metadata.py
+++ scripts/parse_card_metadata.py
@@ -5,7 +5,7 @@ import os
 from pronto import relationship
 metadata_file =  os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
-            "..", "db_metadata",
+            "..",  "chamredb", "data", "db_metadata",
             "card.metadata.obo"
 )
 o = pronto.Ontology(metadata_file)
@@ -21,10 +21,10 @@ for term in o.terms():
     relationships = term.relationships
     for relationship in relationships:
         if relationship.name.startswith('confers_resistance'):
-            antibiotics = ','.join([term.name for term in relationships[relationship]])
+            antibiotics = set([term.name for term in relationships[relationship]])
             if "phenotype" not in metadata[term.id]:
-                metadata[term.id]["phenotype"] = []
-            metadata[term.id]["phenotype"].append(f"{relationship.name.replace('_', ' ')}: {antibiotics}")
+                metadata[term.id]["phenotype"] = {}
+            metadata[term.id]["phenotype"][f"{relationship.name.replace('_', ' ')}"] = antibiotics
 
 # second pass to pull out extra confers resistance from is_a relationships
 for id in metadata:
@@ -32,12 +32,33 @@ for id in metadata:
         is_a_ARO_id = ":".join(metadata[id]["is_a"].split(":")[0:2])
         if "phenotype" in metadata[is_a_ARO_id]:
             if "phenotype" not in metadata[id]:
-                metadata[id]["phenotype"] = []
-            metadata[id]["phenotype"].append(metadata[is_a_ARO_id]["phenotype"])
+                metadata[id]["phenotype"] = {}
+            for phenotype_type in metadata[is_a_ARO_id]["phenotype"]:
+                if phenotype_type in metadata[id]["phenotype"]:
+                    metadata[id]["phenotype"][phenotype_type].update(metadata[is_a_ARO_id]["phenotype"][phenotype_type])
+                else:
+                    metadata[id]["phenotype"][phenotype_type] = metadata[is_a_ARO_id]["phenotype"][phenotype_type]
+
+# finally convert phenotypes to strings
+for id in metadata:
+    if "phenotype" in  metadata[id]:
+        for phenotype_type in metadata[id]["phenotype"]:
+            if phenotype_type not in ["confers resistance to antibiotic", "confers resistance to drug class"]:
+                print(phenotype_type)
+            metadata[id]["phenotype"][phenotype_type] = ",".join(sorted(list(metadata[id]["phenotype"][phenotype_type])))
+        primary_type = "confers resistance to antibiotic"
+        secondary_type = "confers resistance to drug class"
+        if primary_type in metadata[id]["phenotype"]:
+            if secondary_type in metadata[id]["phenotype"]:
+                metadata[id]["additional_phenotype"] = f"{secondary_type}: {metadata[id]['phenotype'][secondary_type]}"
+            metadata[id]["phenotype"] = f"{primary_type}: {metadata[id]['phenotype'][primary_type]}"
+        else:
+            metadata[id]["phenotype"] = f"{secondary_type}: {metadata[id]['phenotype'][secondary_type]}"
+            
 
 out_path =  os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
-            "..", "db_metadata",
+            "..", "chamredb", "data", "db_metadata",
             "card.metadata.json"
 )
 
